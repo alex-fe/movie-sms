9785eefa96873d41ffb60f63b0b41a3fa3838bd9
Alex Feldman <alexfeldman93@gmail.com>
2018-08-03 13:52:43 -0700

    Sends text message

--- app.py
+++ app.py
@@ -1,5 +1,25 @@
-from flask import Flask, Response, request
-from twilio import twiml
+from flask import Flask, Response
+from twilio.twiml.messaging_response import Message, MessagingResponse
 
 
 app = Flask(__name__)
+
+
+@app.route("/")
+def check_app():
+    # returns a simple string stating the app is working
+    return Response("Hello world"), 200
+
+
+@app.route("/twilio", methods=["POST"])
+def inbound_sms():
+    # message_body = request.form['Body']
+    resp = MessagingResponse()
+
+    # replyText = getReply(message_body)
+    resp.message('Hello')
+    return str(resp)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
